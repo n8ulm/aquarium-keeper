7a0cc870007af8a82b71f9d7a0d23661081a44b1
Nate Ulmer <nateulmer@icloud.com>
2019-04-20 17:48:51 -0400

    Add menu

--- app/src/main/java/com/n8ulm/aquariumkeeper/MainActivity.java
+++ app/src/main/java/com/n8ulm/aquariumkeeper/MainActivity.java
@@ -1,19 +1,46 @@
 package com.n8ulm.aquariumkeeper;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 	private TextView mTextMessage;
+
+	private SharedPreferences mSharedPreferences;
+	private GoogleApiClient mGoogleApiClient;
+
+	// Firebase instance variables
+	private FirebaseAuth mFirebaseAuth;
+	private FirebaseUser mFirebaseUser;
+	private DatabaseReference mFirebaseDatabaseReference;
+	private FirebaseRemoteConfig mFirebaseRemoteConfig;
+	private FirebaseAnalytics mFirebaseAnalytics;
+
+	private AdView mAdView;
 
 	private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
 			= new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,9 +67,73 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		// Set default username is anonymous.
+		//mUsername = ANONYMOUS;
+
 		mTextMessage = (TextView) findViewById(R.id.message);
 		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+		mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+		mFirebaseAuth = FirebaseAuth.getInstance();
+		mFirebaseUser = mFirebaseAuth.getCurrentUser();
+		if (mFirebaseUser == null) {
+			// Not signed in, launch the Sign In activity
+			startActivity(new Intent(this, SignInActivity.class));
+			finish();
+			return;
+		} else {
+			//mUsername = mFirebaseUser.getDisplayName();
+			//if (mFirebaseUser.getPhotoUrl() != null) {
+			//	mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+			//}
+		}
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+				.addApi(Auth.GOOGLE_SIGN_IN_API)
+				.build();
+
+		// Initialize Firebase Remote Config.
+		mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
+		// Define Firebase Remote Config Settings.
+		FirebaseRemoteConfigSettings firebaseRemoteConfigSettings =
+				new FirebaseRemoteConfigSettings.Builder()
+						.setDeveloperModeEnabled(true)
+						.build();
+
+
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		// Check if user is signed in.
+		// TODO: Add code to check if user is signed in.
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
 }
--- app/src/main/java/com/n8ulm/aquariumkeeper/SignInActivity.java
+++ app/src/main/java/com/n8ulm/aquariumkeeper/SignInActivity.java
@@ -1,17 +1,28 @@
 package com.n8ulm.aquariumkeeper;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity implements
 		GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
@@ -31,7 +42,11 @@ public class SignInActivity extends AppCompatActivity implements
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_in);
 
+		// Assign Fields
 		mSignInButton = findViewById(R.id.sign_in_button);
+
+		// Set click listeners
+		mSignInButton.setOnClickListener(this);
 
 		// Configure Google Sign In
 		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -42,15 +57,73 @@ public class SignInActivity extends AppCompatActivity implements
 				.enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
 				.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
 				.build();
+
+		// Initialize FirebaseAuth
+		mFirebaseAuth = FirebaseAuth.getInstance();
 	}
 
 	@Override
 	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.sign_in_button:
+				signIn();
+				break;
+		}
 
+	}
+
+	private void signIn() {
+		Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+		startActivityForResult(signInIntent, RC_SIGN_IN);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		// Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+		if (requestCode == RC_SIGN_IN) {
+			GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+			if (result.isSuccess()) {
+				// Google Sign-In was successful, authenticate with Firebase
+				GoogleSignInAccount account = result.getSignInAccount();
+				firebaseAuthWithGoogle(account);
+			} else {
+				// Google Sign-In failed
+				Log.d(TAG, "Google Sign-In Failed");
+			}
+		}
+	}
+
+	private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+		Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+		AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+		mFirebaseAuth.signInWithCredential(credential)
+				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+					@Override
+					public void onComplete(@NonNull Task<AuthResult> task) {
+						Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+						// If sign in fails, display a message to the user. If sign in succeeds
+						// the auth state listener will be notified and logic to handle the
+						// signed in user can be handled in the listener.
+						if (!task.isSuccessful()) {
+							Log.w(TAG, "signInWithCredential", task.getException());
+							Toast.makeText(SignInActivity.this, "Authentication Failed",
+									Toast.LENGTH_SHORT).show();
+						} else {
+							startActivity(new Intent(SignInActivity.this, MainActivity.class));
+							finish();
+						}
+					}
+				});
 	}
 
 	@Override
 	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+	// An unresolvable error has occurred and Google APIs (including Sign-In) will not
+	// be available.
+		Log.d(TAG, "onConnectionFailed:" + connectionResult);
+		Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
 	}
 }
