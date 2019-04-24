f3240203fd773e5f3ba5ee0d64fc4acbb276b4a2
n8ulm <nateulmer@icloud.com>
2019-04-24 14:05:49 -0400

    Add instance variables

--- app/src/main/java/com/n8ulm/aquariumkeeper/ResultInputFragment.java
+++ app/src/main/java/com/n8ulm/aquariumkeeper/ResultInputFragment.java
@@ -11,17 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ResultInputFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ResultInputFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ResultInputFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 	// TODO: Rename parameter arguments, choose names that match
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,19 +30,19 @@ public class ResultInputFragment extends Fragment implements AdapterView.OnItemS
 
 	private OnFragmentInteractionListener mListener;
 
+	// Instance Variable
+	EditText mTestResult;
+	EditText mTestDate;
+
+	Button mSaveResultButton;
+	Button mCancelResultButton;
+
 	public ResultInputFragment() {
 		// Required empty public constructor
 	}
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @param param1 Parameter 1.
-	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment ResultInputFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
+
+
 	public static ResultInputFragment newInstance(String param1, String param2) {
 		ResultInputFragment fragment = new ResultInputFragment();
 		Bundle args = new Bundle();
